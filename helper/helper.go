6207caaa7a51362f31cc29df8b4dd4df34679cb2
altrawan <muhammadalifputrasetiawan@gmail.com>
2022-09-04 14:29:38 +0700

    feat(helper): add config password

---
+++ helper/helper.go
@@ -0,0 +1,26 @@
+package helper
+
+import (
+	"log"
+
+	"golang.org/x/crypto/bcrypt"
+)
+
+func VerifyPassword(hashedPassword string, plainPassword []byte) bool {
+	byteHash := []byte(hashedPassword)
+	err := bcrypt.CompareHashAndPassword(byteHash, plainPassword)
+	if err != nil {
+		log.Println(err)
+		return false
+	}
+	return true
+}
+
+func HashAndSalt(pwd []byte) string {
+	hash, err := bcrypt.GenerateFromPassword(pwd, bcrypt.DefaultCost)
+	if err != nil {
+		log.Println(err)
+		panic("Failed to hash a password")
+	}
+	return string(hash)
+}

3a52d3348a93698e87a7b7c31942be0b8a4b15f0
altrawan <muhammadalifputrasetiawan@gmail.com>
2022-09-04 15:11:08 +0700

    fix: remove sql null value

--- entity/transaction_entity.go
+++ entity/transaction_entity.go
@@ -1,23 +1,21 @@
 package entity
 
 import (
-	"database/sql"
-
 	"gorm.io/gorm"
 )
 
 type Transaction struct {
-	ID             uint64         `gorm:"primary_key:auto_increment" json:"id"`
-	UserID         uint64         `gorm:"not null" json:"user_id"`
-	Invoice        string         `gorm:"type:varchar(255) not null" json:"invoice"`
-	Total          uint64         `gorm:"type:integer not null" json:"total"`
-	PaymentMethod  sql.NullString `gorm:"type:varchar(255)" json:"payment_method"`
-	Status         string         `gorm:"type:varchar(255) not null" json:"status"`
-	RecipientName  sql.NullString `gorm:"type:varchar(255)" json:"recipient_name"`
-	RecipientPhone sql.NullString `gorm:"type:varchar(255)" json:"recipient_phone"`
-	City           sql.NullString `gorm:"type:varchar(255)" json:"city"`
-	Address        sql.NullString `gorm:"type:text" json:"address"`
-	PostalCode     sql.NullInt64  `gorm:"type:integer" json:"postal_code"`
+	ID             uint64 `gorm:"primary_key:auto_increment" json:"id"`
+	UserID         uint64 `gorm:"not null" json:"user_id"`
+	Invoice        string `gorm:"type:varchar(255) not null" json:"invoice"`
+	Total          uint64 `gorm:"type:integer not null" json:"total"`
+	PaymentMethod  string `gorm:"type:varchar(255)" json:"payment_method"`
+	Status         string `gorm:"type:varchar(255) not null" json:"status"`
+	RecipientName  string `gorm:"type:varchar(255)" json:"recipient_name"`
+	RecipientPhone string `gorm:"type:varchar(255)" json:"recipient_phone"`
+	City           string `gorm:"type:varchar(255)" json:"city"`
+	Address        string `gorm:"type:text" json:"address"`
+	PostalCode     uint64 `gorm:"type:integer" json:"postal_code"`
 	gorm.Model
 	User User `gorm:"foreignkey:UserID;constraint:onUpdate:CASCADE,onDelete:CASCADE" json:"user"`
 }

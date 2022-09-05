fd1d15ed4b7cb39d81733daf2a011062c1417c80
altrawan <muhammadalifputrasetiawan@gmail.com>
2022-09-05 08:28:50 +0700

    fix(route): fix route

--- router/router.go
+++ router/router.go
@@ -46,7 +46,7 @@ func NewRouter(db *gorm.DB) *gin.Engine {
 		c.Set("db", db)
 	})
 
-	// addressRepository := repository.NewAddressRepository(db)
+	addressRepository := repository.NewAddressRepository(db)
 	authRepository := repository.NewAuthRepository(db)
 	brandRepository := repository.NewBrandRepository(db)
 	cartRepository := repository.NewCartRepository(db)
@@ -54,8 +54,9 @@ func NewRouter(db *gorm.DB) *gin.Engine {
 	productRepository := repository.NewProductRepository(db)
 	profileRepository := repository.NewProfileRepository(db)
 	storeRepository := repository.NewStoreRepository(db)
+	transactionRepository := repository.NewTransactionRepository(db)
 
-	// addressService := service.NewAddressService(addressRepository)
+	addressService := service.NewAddressService(addressRepository)
 	authService := service.NewAuthService(authRepository)
 	brandService := service.NewBrandService(brandRepository)
 	cartService := service.NewCartService(cartRepository)
@@ -63,8 +64,9 @@ func NewRouter(db *gorm.DB) *gin.Engine {
 	productService := service.NewProductService(productRepository)
 	profileService := service.NewProfileService(profileRepository)
 	storeService := service.NewStoreService(storeRepository)
+	transactionService := service.NewTransactionService(transactionRepository)
 
-	// addressController := controller.NewAddressController(addressService)
+	addressController := controller.NewAddressController(addressService)
 	authController := controller.NewAuthController(authService)
 	brandController := controller.NewBrandController(brandService)
 	cartController := controller.NewCartController(cartService)
@@ -72,6 +74,7 @@ func NewRouter(db *gorm.DB) *gin.Engine {
 	productController := controller.NewProductController(productService)
 	profileController := controller.NewProfileController(profileService)
 	storeController := controller.NewStoreController(storeService)
+	transactionController := controller.NewTransactionController(transactionService)
 
 	r.NoRoute(func(ctx *gin.Context) {
 		ctx.JSON(404, gin.H{"message": "Path not found"})
@@ -83,14 +86,14 @@ func NewRouter(db *gorm.DB) *gin.Engine {
 	// use ginSwagger middleware to serve the API docs
 	v1.GET("/swagger/*any", ginSwagger.WrapHandler(swaggerFiles.Handler))
 
-	// addressRoutes := v1.Group("/address")
-	// {
-	// 	addressRoutes.GET("/address", addressController.List)
-	// 	addressRoutes.GET("/address/:id", addressController.Detail)
-	// 	addressRoutes.POST("/address", addressController.Store)
-	// 	addressRoutes.PUT("/address/:id", addressController.Update)
-	// 	addressRoutes.DELETE("/address/:id", addressController.Delete)
-	// }
+	addressRoutes := v1.Group("/address")
+	{
+		addressRoutes.Use(middleware.JwtAuth()).GET("/", addressController.List)
+		addressRoutes.Use(middleware.JwtAuth()).GET("/:id", addressController.Detail)
+		addressRoutes.Use(middleware.JwtAuth()).POST("/", addressController.Store)
+		addressRoutes.Use(middleware.JwtAuth()).PUT("/:id", addressController.Update)
+		addressRoutes.Use(middleware.JwtAuth()).DELETE("/:id", addressController.Delete)
+	}
 
 	authRoutes := v1.Group("/auth")
 	{
@@ -112,11 +115,11 @@ func NewRouter(db *gorm.DB) *gin.Engine {
 
 	cartRoutes := v1.Group("/cart")
 	{
-		cartRoutes.GET("/cart", cartController.List)
-		cartRoutes.GET("/cart/:id", cartController.Detail)
-		cartRoutes.Use(middleware.JwtAuth()).POST("/cart", cartController.Store)
-		cartRoutes.Use(middleware.JwtAuth()).PUT("/cart/:id", cartController.Update)
-		cartRoutes.Use(middleware.JwtAuth()).DELETE("/cart/:id", cartController.Delete)
+		cartRoutes.Use(middleware.JwtAuth()).GET("/", cartController.List)
+		cartRoutes.Use(middleware.JwtAuth()).GET("/:id", cartController.Detail)
+		cartRoutes.Use(middleware.JwtAuth()).POST("/", cartController.Store)
+		cartRoutes.Use(middleware.JwtAuth()).PUT("/:id", cartController.Update)
+		cartRoutes.Use(middleware.JwtAuth()).DELETE("/:id", cartController.Delete)
 	}
 
 	categoryRoutes := v1.Group("/category")
@@ -150,6 +153,16 @@ func NewRouter(db *gorm.DB) *gin.Engine {
 		storeRoutes.GET("/:id", storeController.Detail)
 		storeRoutes.Use(middleware.JwtAuth()).PUT("/:id", storeController.Update)
 		storeRoutes.Use(middleware.JwtAuth()).PUT("/change-password", storeController.ChangePassword)
+	}
+
+	transactionRoutes := v1.Group("/transaction")
+	{
+		transactionRoutes.Use(middleware.JwtAuth()).GET("/", transactionController.List)
+		transactionRoutes.Use(middleware.JwtAuth()).GET("/:id", transactionController.Detail)
+		transactionRoutes.Use(middleware.JwtAuth()).POST("/", transactionController.Store)
+		transactionRoutes.Use(middleware.JwtAuth()).PUT("/:id/address", transactionController.UpdateAddress)
+		transactionRoutes.Use(middleware.JwtAuth()).PUT("/:id/payment", transactionController.UpdatePayment)
+		transactionRoutes.Use(middleware.JwtAuth()).DELETE("/:id", transactionController.Delete)
 	}
 
 	r.Run(":" + port)

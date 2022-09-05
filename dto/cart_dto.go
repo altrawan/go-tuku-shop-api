a734b816351d0d7ecceb84c9823283b1bcf573a9
altrawan <muhammadalifputrasetiawan@gmail.com>
2022-09-05 08:28:05 +0700

    feat(cart): add endpoint cart

--- dto/cart_dto.go
+++ dto/cart_dto.go
@@ -2,13 +2,15 @@ package dto
 
 // CartCreateDTO is a model that user use when create a new category
 type CartCreateDTO struct {
-	ProductID string `json:"product_id" form:"product_id" binding:"required"`
-	Qty       string `json:"qty" form:"qty" binding:"required"`
+	UserID    uint64 `json:"user_id" form:"user_id"`
+	ProductID uint64 `json:"product_id" form:"product_id" binding:"required"`
+	Qty       uint64 `json:"qty" form:"qty" binding:"required"`
 }
 
 // CartUpdateDTO is a model that user use  when updating a category
 type CartUpdateDTO struct {
 	ID        uint64 `json:"id" form:"id"`
-	ProductID string `json:"product_id" form:"product_id" binding:"required"`
-	Qty       string `json:"qty" form:"qty" binding:"required"`
+	UserID    uint64 `json:"user_id" form:"user_id"`
+	ProductID uint64 `json:"product_id" form:"product_id" binding:"required"`
+	Qty       uint64 `json:"qty" form:"qty" binding:"required"`
 }
